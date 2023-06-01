de9cb22eec4fd2643cd5be741c20a14b02827b31
Reniactor <reniactor@hotmail.com>
2023-06-01 16:31:21 -0400

    fixed typo

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,7 +1,7 @@
 const Custom404 = () => {
   return (
     <div className="min-h-screen min-w-screen flex justify-center items-center text-red-500 font-medium">
-      You don't have access to this page.
+      You dont have access to this page.
     </div>
   );
 };
