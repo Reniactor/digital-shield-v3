263184b764e1aa1936412f45b818c18ca6e92ade
Reniactor <reniactor@hotmail.com>
2023-06-07 16:29:33 -0400

    applied chain enforcer

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,12 @@
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import Head from "next/head";
+import { Polygon } from "@thirdweb-dev/chains";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "polygon";
+const activeChain = Polygon;
 
 function MyApp({ Component, pageProps }) {
   return (
